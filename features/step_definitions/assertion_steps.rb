2fdf37c080e165578b1da1c2cfbc5aa45f9c352c
vick3d <viktoro0090@gmail.com>
2019-02-19 19:50:23 +0100

    refactor feature to check information within div of products.

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,12 @@
+  Then("I should see {string} for {string}") do |info, product|
+    dish = Product.find_by(name: product)
+    dom_section = "#product_#{dish.id}"
+    within(dom_section) do 
+      expect(page).to have_content info
+    end
+  end  
+
   Then("I should see {string}") do |content|
     expect(page).to have_content content
-  end  
+  end
+  
