d346679c799bf7aeefe5d51bef59bfadc80e71ae
Per Eriksson <per_hegg@hotmail.com>
2019-02-20 10:24:39 +0100

    fix minor indentation and restructured basic steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,16 +1,9 @@
-When("I click on {string}") do |button_name|
-    click_on button_name
+Given("the following products exists") do |table|
+  table.hashes.each do |product|
+    FactoryBot.create(:product, product)
+  end 
 end
 
-When("I fill in {string} with {string}") do |field, input|
-    fill_in field, with: input
-end
-  Given("the following products exists") do |table|
-    table.hashes.each do |product|
-      FactoryBot.create(:product, product)
-    end 
-  end
-  
 When("I am on the {string} page") do |page|
   case page
   when "product" 
@@ -19,3 +12,11 @@ When("I am on the {string} page") do |page|
     return false 
   end
 end  
+
+When("I click on {string}") do |button_name|
+    click_on button_name
+end
+
+When("I fill in {string} with {string}") do |field, input|
+    fill_in field, with: input
+end
