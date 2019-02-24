092530e9264a983b8b62a15718d2ff5b1e2a0eaf
vick3d <viktoro0090@gmail.com>
2019-02-24 22:21:10 +0100

    adds navbar and basic styling

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'turbolinks', '~> 5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise'
+gem 'webpacker', '~> 3.0'
 
 group :development, :test do
   gem 'pry-byebug'
---
+++ public/packs/application-c9029395a9d8c04eb1b9.js
@@ -0,0 +1,102 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!*********************************************!*\
+  !*** ./app/javascript/packs/application.js ***!
+  \*********************************************/
+/*! no exports provided */
+/*! all exports used */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_application_css__ = __webpack_require__(/*! application.css */ 1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_application_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_application_css__);
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+
+
+/***/ }),
+/* 1 */
+/*!***********************************************!*\
+  !*** ./app/javascript/styles/application.css ***!
+  \***********************************************/
+/*! dynamic exports provided */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Syntax Error \n\n(15:5) Unknown word\n\n  13 | \n  14 | .signup:hover {\n> 15 |     border-transparent text-teal bg-white\n     |     ^\n  16 | }\n\n    at runLoaders (/home/vick3d/slowfood-jan2019-team2/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/vick3d/slowfood-jan2019-team2/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/vick3d/slowfood-jan2019-team2/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/vick3d/slowfood-jan2019-team2/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Promise.resolve.then.then.catch (/home/vick3d/slowfood-jan2019-team2/node_modules/postcss-loader/lib/index.js:194:44)");
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=application-c9029395a9d8c04eb1b9.js.map
